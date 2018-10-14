a3c3b541af5086458d9730967dcbeb37de8dcc1c
Konstantin Lynda <knlynda@gmail.com>
2018-10-14 14:48:40 +0200

    Extend Status values.

--- lib/status_page_ruby/status.rb
+++ lib/status_page_ruby/status.rb
@@ -1,15 +1,20 @@
 module StatusPageRuby
   class Status
-    attr_reader :service, :state, :time
+    attr_reader :service, :state, :status, :time
 
-    def initialize(service, state, time)
+    def initialize(service, state, status, time)
       @service = service.to_s
       @state = state.to_s
+      @status = status.to_s
       @time = time.to_s
     end
 
-    def record
+    def history_record
       [service, state, time]
+    end
+
+    def record
+      [service, state, status, time]
     end
 
     def to_csv
--- spec/lib/status_page_ruby/status_spec.rb
+++ spec/lib/status_page_ruby/status_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 RSpec.describe StatusPageRuby::Status do
   describe '.new' do
     [
-      [nil, nil, nil],
-      ['', '', ''],
-      [1, 2, 3],
-      ['Github', 'up', 1_539_506_590]
-    ].each do |service, state, time|
-      context "with service: #{service.inspect}, state: #{state.inspect} and time: #{time.inspect}" do
-        subject { described_class.new(service, state, time) }
+      [nil, nil, nil, nil],
+      ['', '', '', ''],
+      [1, 2, 2, 3],
+      ['Github', 'up', 'All Good', 1_539_506_590]
+    ].each do |service, state, status, time|
+      context "with service: #{service}, state: #{state}, status: #{status} and time: #{time}" do
+        subject { described_class.new(service, state, status, time) }
 
         it { expect(subject.service).to eq(service.to_s) }
         it { expect(subject.state).to eq(state.to_s) }
@@ -20,27 +20,27 @@ RSpec.describe StatusPageRuby::Status do
 
   describe '#record' do
     [
-      [nil, nil, nil],
-      ['', '', ''],
-      [1, 2, 3],
-      ['Github', 'up', 1_539_506_590]
-    ].each do |service, state, time|
-      context "with service: #{service.inspect}, state: #{state.inspect} and time: #{time.inspect}" do
-        subject { described_class.new(service, state, time).record }
+      [nil, nil, nil, nil],
+      ['', '', '', ''],
+      [1, 2, 3, 4],
+      ['Github', 'up', 'All Good', 1_539_506_590]
+    ].each do |service, state, status, time|
+      context "with service: #{service}, state: #{state}, status: #{status} and time: #{time}" do
+        subject { described_class.new(service, state, status, time).record }
 
-        it { is_expected.to eq([service.to_s, state.to_s, time.to_s]) }
+        it { is_expected.to eq([service.to_s, state.to_s, status.to_s, time.to_s]) }
       end
     end
   end
 
   describe '#to_csv' do
     [
-      [[nil, nil, nil],                 "\"\",\"\",\"\"\n"],
-      [['', '', ''],                    "\"\",\"\",\"\"\n"],
-      [[1, 2, 3],                       "1,2,3\n"],
-      [['Github', 'up', 1_539_506_590], "Github,up,1539506590\n"]
+      [[nil, nil, nil, nil],                        "\"\",\"\",\"\",\"\"\n"],
+      [['', '', '', ''],                            "\"\",\"\",\"\",\"\"\n"],
+      [[1, 2, 3, 4],                                "1,2,3,4\n"],
+      [['Github', 'up', 'All Good', 1_539_506_590], "Github,up,All Good,1539506590\n"]
     ].each do |args, result|
-      context "with service: #{args[0].inspect}, state: #{args[1].inspect} and time: #{args[2].inspect}" do
+      context "with service: #{args[0]}, state: #{args[1]}, status: #{args[2]} and time: #{args[3]}" do
         subject { described_class.new(*args).to_csv }
 
         it { is_expected.to eq(result) }
